bc369308439af8c8e9c4d9f1a7e5d13a405dd5f0
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-11-19 14:13:12 -0800

    adding notes and assignments solutions

--- w8/assignment/7sCDOPzNR8uAgzj8zXfL7A_4f8515568abe4b3b980175853bd0fcf1_ex7-octave/findClosestCentroids.m
+++ w8/assignment/7sCDOPzNR8uAgzj8zXfL7A_4f8515568abe4b3b980175853bd0fcf1_ex7-octave/findClosestCentroids.m
@@ -22,10 +22,17 @@ idx = zeros(size(X,1), 1);
 %
 
 
-
-
-
-
+for i = 1:size(X,1)
+	min_d = inf;
+	for k = 1:K
+		diff = X(i, :)'-centroids(k, :)';
+		d = diff'*diff;
+		if (d < min_d)
+			idx(i) = k;
+			min_d = d;
+		end
+	end
+end
 
 % =============================================================
 
